29a8468d1056a70d2519d4fc907d9d9d27b7ba32
WINIFRED NNAMDI <winifred.nnamdi@TEACHFORNIGERIA.COM>
2022-10-19 13:05:35 +0100

    This is my first commit

---
+++ week1_day1/hello_world.py
@@ -0,0 +1,30 @@
+# print("Hello World")
+# print("Toyin's world")
+# multiline_strings = '''
+# life is beautiful
+# game on
+# python
+# '''
+# print(multiline_strings)
+# my_dict = {"name_1": "Thelma", "age": 40, "hobbies": ["reading","singing", "coding"],"my_multi_line": multiline_strings}
+# print(my_dict)
+# print(my_dict["age"])
+# print(my_dict["hobbies"])
+# print(my_dict["my_multi_line"])
+
+# my_list = ["apple","organge","banana"]
+# print(my_list)
+# print(my_list[1])
+
+# # mutating values in the list
+# my_list[2] = "mangoes"
+# print(my_list[2])
+
+my_tuple = ("apple","organge","banana")
+# print(my_tuple)
+
+# my_tuple[2] = "mangoes"
+# print(my_tuple[2])
+
+print(type
+(my_tuple))
---
+++ week1_day2/control_flow.py
@@ -0,0 +1,22 @@
+# if statement practice 
+from re import X
+
+
+x = int(input("Please input your grade:"))
+if type(x) == int:
+   
+    if x >= 70:     
+        print("A")
+    elif x >= 60 and x <= 69:
+        print("B")
+    elif x >= 50 and x <= 59:
+        print("C")
+    elif x >= 40 and x <= 49:
+        print("D")
+    else:
+        print("Failed")
+
+else:
+    print("Our program only accepts integer values")
+
+print("I will execute either way")
